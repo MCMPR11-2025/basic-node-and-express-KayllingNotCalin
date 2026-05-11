1c02cc9db409610f76688d66697f986118042359
KayllingNotCalin <1400604@learn.vsb.bc.ca>
2026-05-11 20:35:36 +0000

    hi

--- myApp.js
+++ myApp.js
@@ -1,15 +1,11 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config
 console.log("Hello World");
 
-
-
-
-
-
-
-
+app.get("/", function(req, res) {
+    res.send("Hello Express")
+});
 
 
 
