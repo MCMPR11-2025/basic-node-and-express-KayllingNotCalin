7e9933a3d294c526ba7229e6b4d05b8f7e2e991d
KayllingNotCalin <1400604@learn.vsb.bc.ca>
2026-05-14 05:05:13 +0000

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- myApp.js
+++ myApp.js
@@ -1,7 +1,20 @@
 let express = require('express');
 let app = express();
-require('dotenv').config
+// require('dotenv').config
 console.log("Hello World");
+
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 app.get("/", function(req, res) {
     res.send("Hello Express")
@@ -29,7 +42,5 @@ app.get("/", function(req, res) {
 
 
 
-
-
-
  module.exports = app;
+// node server.js
